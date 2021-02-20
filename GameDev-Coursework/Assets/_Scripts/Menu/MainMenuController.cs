46fb244bde7932b74b0ba2c906c96c469d7117ba
leeder.josh <leederjl@aston.ac.uk>
2021-02-20 17:47:13 +0000

    Main menu added with single and multiplayer buttons. Buttons linked to code but not functional

---
+++ GameDev-Coursework/Assets/_Scripts/Menu/MainMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void OnClickSinglePlayer()
+    {
+        Debug.Log("Single player pressed");
+    }
+
+    public void OnClickMultilayer()
+    {
+        Debug.Log("Multi player pressed");
+    }
+}

d7b38b0ec8b427a374054ee559af2a64e3f43257
leeder.josh <leederjl@aston.ac.uk>
2021-02-20 18:17:03 +0000

    Menus all hooked up and working correctly

--- GameDev-Coursework/Assets/_Scripts/Menu/MainMenuController.cs
+++ GameDev-Coursework/Assets/_Scripts/Menu/MainMenuController.cs
@@ -1,16 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    public void OnClickSinglePlayer()
+    private const int mainMenuIndex = 0;
+    private const int singlePlayerMenuIndex = 1;
+    private const int lavaLevelIndex = 2;
+
+    public IEnumerator LoadScene(int sceneIndex)
     {
-        Debug.Log("Single player pressed");
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 
-    public void OnClickMultilayer()
+    public void MainMenuSinglePlayerToSinglePlayerMenu()
+    {
+        Debug.Log("Single player pressed");
+
+        StartCoroutine(LoadScene(singlePlayerMenuIndex));
+    }
+
+    public void MainMenuMultiPlayerToMultiplayer()
     {
         Debug.Log("Multi player pressed");
+    }
+
+    public void SinglePlayerToMainMenu()
+    {
+        Debug.Log("Single player to main menu");
+
+        StartCoroutine(LoadScene(mainMenuIndex));
+    }
+
+    public void SinglePlayerToLavaLevel()
+    {
+        Debug.Log("Single player to lava level");
+
+        StartCoroutine(LoadScene(lavaLevelIndex));
+    }
+
+    public void SinglePlayerToSecondLevel()
+    {
+        Debug.Log("Single player to second level");
+    }
+
+    public void SinglePlayerToThirdLevel()
+    {
+        Debug.Log("Single player to third level");
     }
 }

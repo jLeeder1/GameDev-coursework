f35152676075514f4b8f010e3a69f48184698c45
leeder.josh <leederjl@aston.ac.uk>
2021-02-26 18:22:12 +0000

    Initial Navmesh in. Still some kinks in the fallen building area

---
+++ GameDev-Coursework/Assets/_Scripts/TestNavMeshNPC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestNavMeshNPC : MonoBehaviour
+{
+    [SerializeField]
+    GameObject targetGameObject;
+
+    private NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(targetGameObject.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

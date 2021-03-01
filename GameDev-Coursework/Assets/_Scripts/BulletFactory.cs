2609f3b7b567f23f7dc571a04b88882b848fdfe6
leeder.josh <leederjl@aston.ac.uk>
2021-03-01 13:39:42 +0000

    Addult rifle added and working, also added skybox to lavaLevel

---
+++ GameDev-Coursework/Assets/_Scripts/BulletFactory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class BulletFactory : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bullet;
+
+    [SerializeField]
+    private GameObject bulletSpawn;
+
+    [SerializeField]
+    private float bulletThrust;
+
+    private FirstPersonController firstPersonController;
+    private Camera firstPersonControllerCamera;
+
+    private void Start()
+    {
+        firstPersonController = GetComponentInParent<FirstPersonController>();
+        firstPersonControllerCamera = GetComponentInParent<Camera>();
+    }
+
+    public void InstantiateBullet()
+    {
+        GameObject bulleltInstance = Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        Rigidbody bulletRigidbody = bulleltInstance.GetComponent<Rigidbody>();
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray))
+        {
+            bulletRigidbody.AddForce(ray.direction * bulletThrust);
+        }
+    }
+}
---
+++ GameDev-Coursework/Assets/_Scripts/BulletLifetime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BulletLifetime : MonoBehaviour
+{
+    [SerializeField]
+    private int bulletLifeInSeconds;
+
+    void Start()
+    {
+        StartCoroutine(DestroyBulletAfterGivenTime());
+    }
+
+    IEnumerator DestroyBulletAfterGivenTime()
+    {
+        yield return new WaitForSeconds(bulletLifeInSeconds);
+        Destroy(this.gameObject);
+    }
+}

413e2ddfde884819b72f27979d1c2f7a694f1ca2
leeder.josh <leederjl@aston.ac.uk>
2021-03-22 20:18:54 +0000

    Improved jump animation and added animation events

---
+++ GameDev-Coursework/Assets/_Scripts/NPC/NPCAnimationHandler.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCAnimationHandler : MonoBehaviour
+{
+    public bool IsOnOffMeshLink { get; private set; }
+
+    private NavMeshAgent navMeshAgent;
+    private float currentSpeed;
+
+    // Animations
+    private Animator animator;
+    private int walkingSpeedAnimatorId;
+    private int isJumpingUpId;
+    private int jumpAnimatorId;
+
+    // Cool down
+    private int isJumpingUpCoolDownTimer = 2;
+    private bool isJumpingUpCoolDownActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.autoTraverseOffMeshLink = false;
+        animator = GetComponent<Animator>();
+        SetAnimatorParamaters();
+    }
+
+    private void SetAnimatorParamaters()
+    {
+        walkingSpeedAnimatorId = Animator.StringToHash("walkingSpeed");
+        isJumpingUpId = Animator.StringToHash("isJumpingUp");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentSpeed = navMeshAgent.velocity.magnitude;
+        animator.SetFloat(walkingSpeedAnimatorId, currentSpeed);
+
+        if (!navMeshAgent.isOnOffMeshLink)
+        {
+            return;
+        }
+
+        HandleNPCJumpingUpLedge();
+        //IsNavMeshAgentOnNavMeshOffLink();
+    }
+
+    private void IsNavMeshAgentOnNavMeshOffLink()
+    {
+        animator.SetBool(isJumpingUpId, navMeshAgent.isOnOffMeshLink);
+    }
+
+    private void HandleNPCJumpingUpLedge()
+    {
+        if(navMeshAgent.currentOffMeshLinkData.endPos.y > navMeshAgent.currentOffMeshLinkData.startPos.y && !isJumpingUpCoolDownActive)
+        {
+            animator.SetBool(isJumpingUpId, true);
+        }
+    }
+
+    public void CompleteOffNavMeshLink()
+    {
+        navMeshAgent.CompleteOffMeshLink();
+    }
+
+    public void ResetIsJumpingUp()
+    {
+        Debug.Log("Reset is jumping up test");
+        animator.SetBool(isJumpingUpId, false);
+        StartCoroutine(IsJumpingUpCoolDown());
+    }
+
+    IEnumerator IsJumpingUpCoolDown()
+    {
+        isJumpingUpCoolDownActive = true;
+        yield return new WaitForSeconds(isJumpingUpCoolDownTimer);
+        isJumpingUpCoolDownActive = false;
+    }
+}
--- GameDev-Coursework/Assets/_Scripts/NPC/NPCMovement.cs
+++ GameDev-Coursework/Assets/_Scripts/NPC/NPCMovement.cs
@@ -5,47 +5,26 @@ using UnityEngine.AI;
 
 public class NPCMovement : MonoBehaviour
 {
-    public bool IsOnOffMeshLink { get; private set; }
-    private bool isMovementSetToChasePlayer;
     private NavMeshAgent navMeshAgent;
     private string opposingTeamTag;
-
-    private float currentSpeed;
-    private float stoppingDistance = 2f;
+    private float stoppingDistance;
     private int updateTargetCoolDownInSeconds = 3;
-
-    // Animations
-    private Animator animator;
-    private int walkingSpeedAnimatorId;
-    private int isJumpingUpId;
-    private int jumpAnimatorId;
     private bool isUpdateTargetCoolDownActive = false;
 
     void Start()
     {
-        isMovementSetToChasePlayer = true;
         navMeshAgent = GetComponent<NavMeshAgent>();
-        navMeshAgent.stoppingDistance = stoppingDistance;
         GetOpposingTeamTag();
         SetDestinationToRandomOpposingTeamMember(); // Will need to take this out of start
-
-        animator = GetComponent<Animator>();
-        walkingSpeedAnimatorId = Animator.StringToHash("walkingSpeed");
-        isJumpingUpId = Animator.StringToHash("isJumpingUp");
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentSpeed = navMeshAgent.velocity.magnitude;
-        animator.SetFloat(walkingSpeedAnimatorId, currentSpeed);
-
         if (!isUpdateTargetCoolDownActive)
         {
             SetDestinationToRandomOpposingTeamMember();
         }
-
-        IsNavMeshAgentOnNavMeshOffLink();
     }
 
     private void GetOpposingTeamTag()
@@ -70,10 +49,5 @@ public class NPCMovement : MonoBehaviour
         isUpdateTargetCoolDownActive = true;
         yield return new WaitForSeconds(updateTargetCoolDownInSeconds);
         isUpdateTargetCoolDownActive = false;
-    }
-
-    private void IsNavMeshAgentOnNavMeshOffLink()
-    {
-        animator.SetBool(isJumpingUpId, navMeshAgent.isOnOffMeshLink);
     }
 }
